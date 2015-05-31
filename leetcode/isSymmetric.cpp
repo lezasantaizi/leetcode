0f770ec94131e16b2a405d27eff832a3a46c668f
rxm24217 <xiaomingx.ren@intel.com>
2015-05-31 23:11:08 +0800

    optimization

--- leetcode/isSymmetric.cpp
+++ leetcode/isSymmetric.cpp
@@ -1,14 +1,17 @@
 #include"isSymmetric.h"
 
-TreeNode* symmetric(TreeNode* root)
+bool symmetric(TreeNode* left,TreeNode* right)
 {
-	if(root == NULL)
-		return NULL;
-	TreeNode* left = symmetric(root->left);
-	TreeNode* right = symmetric(root->right);
-	root->left = right;
-	root->right = left;
-	return root;
+	if (left == NULL && right== NULL)
+	{
+		return true;
+	}
+	if (left == NULL || right == NULL)
+	{
+		return false;
+	}
+
+	return (left->val == right->val) &&symmetric(left->left,right->right) && symmetric(left->right,right->left);
 }
 
 bool Solution15::isSymmetric(TreeNode* root)
@@ -17,39 +20,5 @@ bool Solution15::isSymmetric(TreeNode* root)
 	{
 		return true;
 	}
-	TreeNode* newLeft = symmetric(root->left);
-	queue<TreeNode*> tempLeft;
-	queue<TreeNode*> tempRight;
-	if(root->left!=NULL)
-		tempLeft.push(root->left);
-	if(root->right!=NULL)
-		tempRight.push(root->right);
-	while(tempLeft.size()!=0 && tempRight.size()!= 0)
-	{
-		TreeNode* leftFront = tempLeft.front();
-		TreeNode* RightFront = tempRight.front();
-		tempLeft.pop();
-		tempRight.pop();
-		if((leftFront->left ==NULL && RightFront->left!=NULL) || (leftFront->left !=NULL && RightFront->left==NULL)||(leftFront->val != RightFront->val))
-			return false;
-		if(leftFront->left !=NULL)
-		{
-			tempLeft.push(leftFront->left);
-			tempRight.push(RightFront->left);
-		}
-
-		if((leftFront->right ==NULL && RightFront->right!=NULL) || (leftFront->right !=NULL && RightFront->right==NULL))
-			return false;
-		if (leftFront->right != NULL)
-		{
-			tempLeft.push(leftFront->right);
-			tempRight.push(RightFront->right);
-		}
-
-	}
-	if (tempLeft.size() != tempRight.size())
-	{
-		return false;
-	}
-	return true;
+	return symmetric(root->left,root->right);
 }
