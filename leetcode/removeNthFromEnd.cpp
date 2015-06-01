f211e7ade66efe7dfac131ca6c056a7f2a6b5f04
Xiaoming Ren <renxiaoming@smartdevices.com.cn>
2015-06-01 23:06:17 +0800

    Given a linked list, remove the nth node from the end of list and return its head.
    For example,
    
       Given linked list: 1->2->3->4->5, and n = 2.
    
       After removing the second node from the end, the linked list becomes 1->2->3->5.
    Note:
    Given n will always be valid.
    Try to do this in one pass.

---
+++ leetcode/removeNthFromEnd.cpp
@@ -0,0 +1,6 @@
+#include"removeNthFromEnd.h"
+
+ListNode* Solution19::removeNthFromEnd(ListNode* head, int n) 
+{
+
+}
---
+++ leetcode/removeNthFromEnd.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution19 {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+	}
+};
