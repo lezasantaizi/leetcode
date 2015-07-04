d5539990fca0790cdd88ef068e28ad70894ad7e7
rxm24217 <xiaomingx.ren@intel.com>
2015-07-05 00:14:01 +0800

    another method for numTrees

--- leetcode/numTrees.cpp
+++ leetcode/numTrees.cpp
@@ -21,9 +21,25 @@ int func(int* saveTemp,int n)
 }
 int Solution68::numTrees(int n) 
 {
+	//方法1：
+	//int* saveTemp = (int*)malloc(sizeof(int)*(n+1));
+	//memset(saveTemp,0,sizeof(int)*(n+1));
+	//int result =  func(saveTemp,n);
+	//free(saveTemp);
+	//return result;
+
+	//方法2：
 	int* saveTemp = (int*)malloc(sizeof(int)*(n+1));
 	memset(saveTemp,0,sizeof(int)*(n+1));
-	int result =  func(saveTemp,n);
-	free(saveTemp);
-	return result;
+	saveTemp[0] = 1;
+	for (int i = 1 ;i<= n; i++)
+	{
+		int sum = 0;
+		for (int j = 0 ;j< i;j++)
+		{
+			sum +=  saveTemp[j] * saveTemp[i - j -1];
+		}
+		saveTemp[i] = sum;
+	}
+	return saveTemp[n];
 }
