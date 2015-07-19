a97904f86b4fc2788747ab69be6d19811593d0e4
rxm24217 <xiaomingx.ren@intel.com>
2015-07-20 00:54:16 +0800

    the others solution

--- leetcode/recoverTree.cpp
+++ leetcode/recoverTree.cpp
@@ -18,12 +18,87 @@ void func(TreeNode* root)
 
 void Solution65::recoverTree(TreeNode* root) 
 {
-	 temp = new TreeNode(-10000);
-	 wrong1 = NULL;
-	 wrong2 = NULL;
-	 func(root);
-	 int temp_value = wrong1->val;
-	 wrong1->val = wrong2->val;
-	 wrong2->val = temp_value;
-	 return ;
+	//方法1：
+	 //temp = new TreeNode(INT_MIN);
+	 //wrong1 = NULL;
+	 //wrong2 = NULL;
+	 //func(root);
+	 //int temp_value = wrong1->val;
+	 //wrong1->val = wrong2->val;
+	 //wrong2->val = temp_value;
+
+	//方法2：
+	//stack<TreeNode*> saveNode; 
+	//TreeNode* error_node1 = NULL, *error_node2 = NULL, *temp_node = NULL;
+	//int cnt = 0;
+	//temp_node = new TreeNode(INT_MIN);
+	//while (root || saveNode.empty() != true)
+	//{
+	//	while (root)
+	//	{
+	//		saveNode.push(root);
+	//		root = root->left;
+	//	}
+
+	//	TreeNode* temp = saveNode.top();
+	//	if(temp->val < temp_node->val)
+	//	{
+	//		if(error_node1 == NULL)
+	//			error_node1 = temp_node;
+	//		error_node2 = temp;
+	//		cnt++;
+	//		if(cnt == 2)
+	//			break;
+	//	}
+	//	temp_node = temp;
+	//	saveNode.pop();
+	//	root = temp->right;
+
+	//}
+	//int temp_value = error_node1->val;
+	//error_node1->val = error_node2->val;
+	//error_node2->val = temp_value;
+
+
+
+////方法3：
+	TreeNode* cur = root;
+	TreeNode* pre = NULL;
+	TreeNode* wrong1 = NULL;
+	TreeNode* wrong2 = NULL;
+	while(cur!=NULL) {
+		if(cur->left!=NULL) {
+			TreeNode* temp = cur->left;
+			while(temp->right!=NULL && temp->right!=cur)
+				temp = temp->right;
+			if(temp->right==NULL) {
+				temp->right = cur;
+				cur = cur->left;
+			}
+			else {
+				temp->right=NULL;
+				//print
+				if(pre!=NULL && pre->val>cur->val) {
+					if(wrong1==NULL)
+						wrong1=pre;
+					wrong2 = cur;
+				}
+				pre = cur;
+				cur=cur->right;
+			}
+		}
+		else {
+			//print
+			if(pre!=NULL && pre->val>cur->val) {
+				if(wrong1==NULL)
+					wrong1=pre;
+				wrong2 = cur;
+			}
+			pre = cur;
+			cur=cur->right;
+		}
+	}
+	int t = wrong1->val;
+	wrong1->val = wrong2->val;
+	wrong2->val = t;
 }
