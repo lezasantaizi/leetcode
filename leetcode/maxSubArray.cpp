f33e237c7f0894155a628ea6a5dc086d10863564
rxm24217 <xiaomingx.ren@intel.com>
2015-08-02 17:58:24 +0800

    divide and conquer

--- leetcode/maxSubArray.cpp
+++ leetcode/maxSubArray.cpp
@@ -1,5 +1,35 @@
 #include "maxSubArray.h"
 
+int maxSubArrayHelper(vector<int> nums,int l,int r)
+{
+	if (l > r)
+		return -2147483647;
+	int m = (l+r) / 2;
+
+		int leftMax = 0;
+		int sumNum = 0;
+		for (int i = m-1;i>= l;i--) 
+		{
+			sumNum += nums[i];
+			leftMax = max(leftMax, sumNum);
+		}
+
+
+		int rightMax = sumNum = 0;
+		for (int i = m+1;i<= r;i++)
+		{
+			sumNum += nums[i];
+			rightMax = max(rightMax, sumNum);
+		}
+
+
+		int leftAns = maxSubArrayHelper(nums, l, m - 1);
+		int rightAns = maxSubArrayHelper(nums, m + 1, r);
+
+		int result =  max(leftMax + nums[m] + rightMax, max(leftAns, rightAns));
+		return result;
+};
+
 int Solution108::maxSubArray(vector<int>& nums) 
 {
 	//方法1：
@@ -27,15 +57,19 @@ int Solution108::maxSubArray(vector<int>& nums)
 	//return result;
 
 	//方法2：
-	int sum = 0;
-	int result = INT_MIN;
-	for (int i = 0;i< nums.size();i++)
-	{
-		sum = sum > 0 ? sum+nums[i] : nums[i];
-		if(result < sum)
-		{
-			result = sum;
-		}
-	}
-	return result;     
+	//int sum = 0;
+	//int result = INT_MIN;
+	//for (int i = 0;i< nums.size();i++)
+	//{
+	//	sum = sum > 0 ? sum+nums[i] : nums[i];
+	//	if(result < sum)
+	//	{
+	//		result = sum;
+	//	}
+	//}
+	//return result;     
+
+	//方法3：divide and conquer
+	int result =  maxSubArrayHelper(nums, 0, nums.size() - 1);
+	return result;
 }
